33f760774b2aaaf2dae868144c3b7badfb3f743e
rnick1 <rnick0260@gmail.com>
2021-03-11 14:43:18 -0800

    Improved database seed and database.

--- seed.sql
+++ seed.sql
@@ -1,3 +1,4 @@
+USE employeeDB;
 -- Seed data for departments table:
 INSERT INTO departments (depName)
 VALUES ('Finance');
@@ -10,13 +11,13 @@ VALUES ('Sales');
 
 -- Seed data for role table:
 INSERT INTO role (title, salary)
-VALUES ('accountant', 45000.00);
+VALUES ('Junior Manager', 40000.00);
 
 INSERT INTO role (title, salary)
-VALUES ('Assistant HR', 47000.00);
+VALUES ('Manager', 47000.00);
 
 INSERT INTO role (title, salary)
-VALUES ('Sales Manager', 41000.00);
+VALUES ('Senior Manager', 52000.00);
 
 -- Seed data for employee table:
 INSERT INTO employee (first_name, last_name)
