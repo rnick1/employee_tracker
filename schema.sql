6a9a0466cadc8bf55b1327bc6d0365789b92ab5b
rnick1 <rnick0260@gmail.com>
2021-03-12 11:16:23 -0800

    Refactored sql files.

--- schema.sql
+++ schema.sql
@@ -2,36 +2,29 @@ DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 USE employeeDB;
 
+-- Department table:
 CREATE TABLE department (
 id INT NOT NULL AUTO_INCREMENT,
-depName VARCHAR (30),
+name VARCHAR (30),
 PRIMARY KEY (id)
 );
-
-CREATE TABLE empRole (
+-- Role table:
+CREATE TABLE role (
 id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR (30) NOT NULL,
 salary DECIMAL (7, 2) NOT NULL,
-depID INT NOT NULL,
-PRIMARY KEY (id)
+dep_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (dep_id) REFERENCES department (id)
 );
-
+-- Employee table:
 CREATE TABLE employee (
 id INT NOT NULL AUTO_INCREMENT,
 first_name VARCHAR (30) NOT NULL,
 last_name VARCHAR (30) NOT NULL,
-roleID INT NOT NULL,
-manID INT NOT NULL,
-PRIMARY KEY (id)
+role_id INT NOT NULL,
+manager_id INT NOT NULL DEFAULT 0,
+PRIMARY KEY (id),
+FOREIGN KEY (role_id) REFERENCES role (id),
+FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
-
-SELECT * FROM department;
-SELECT * FROM empRole;
-SELECT * FROM employee;
-
--- I need to join department.id with role.depID so that role.depID = department.id
--- I need to join role.id with with employee.roleID so that role.id = employee.roleID
--- When prompted, if the name the user adds as an empoyee's manager, then that manager's employee.id needs to be joined with the employee's employee.manID so that employee.manID = employee.ID (of the manager)
-SELECT id, title, salary
-FROM department
-INNER JOIN role ON department.id = role.depID;
--- seed.sql
+++ seed.sql
@@ -1,30 +1,26 @@
 USE employeeDB;
 -- Seed data for departments table:
-INSERT INTO departments (depName)
-VALUES ('Finance');
+INSERT INTO department (name)
+VALUES ('Finance'), ('HR'), ('Sales');
 
-INSERT INTO departments (depName)
-VALUES ('HR');
-
-INSERT INTO departments (depName)
-VALUES ('Sales');
-
+USE employeeDB;
 -- Seed data for role table:
-INSERT INTO empRole (title, salary)
-VALUES ('Junior Manager', 40000.00);
+INSERT INTO role (title, salary)
+VALUES ('Junior Manager', 40000.00), ('Manager', 47000.00), ('Senior Manager', 52000.00);
 
-INSERT INTO empRole (title, salary)
-VALUES ('Manager', 47000.00);
-
-INSERT INTO empRole (title, salary)
-VALUES ('Senior Manager', 52000.00);
-
+USE employeeDB;
 -- Seed data for employee table:
 INSERT INTO employee (first_name, last_name)
-VALUES ('Phil', 'Smetona');
+VALUES ('Phil', 'Smetona'), ('Harriet', 'Johnson'), ('Arnold', 'Srikar');
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('Harriet', 'Johnson');
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('Arnold', 'Srikar');
+
+-- I need to join department.id with role.depID so that role.depID = department.id
+-- I need to join role.id with with employee.roleID so that role.id = employee.roleID
+-- When prompted, if the name the user adds as an empoyee's manager, then that manager's employee.id needs to be joined with the employee's employee.manID so that employee.manID = employee.ID (of the manager)
+SELECT id, title, salary
+FROM department
+INNER JOIN role ON department.id = role.depID;
