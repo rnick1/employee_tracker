18da99dd5c632329508f9e3ddaab5f1d2ae2356b
rnick1 <rnick0260@gmail.com>
2021-03-13 09:57:15 -0800

    Fixed issue where seed information was not populating my tables in the schema.

--- schema.sql
+++ schema.sql
@@ -4,29 +4,28 @@ USE employeeDB;
 
 -- Department table:
 CREATE TABLE department (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR (30) NOT NULL,
-PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
 );
 -- Role table:
 CREATE TABLE role (
-id INT NOT NULL AUTO_INCREMENT,
-title VARCHAR (30) NOT NULL,
-salary DECIMAL (7, 2) NOT NULL,
-dep_id INT NOT NULL,
-PRIMARY KEY (id),
-FOREIGN KEY (dep_id) REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL (7, 2) NOT NULL,
+    dep_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (dep_id) REFERENCES department(id)
 );
 -- Employee table:
 CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT,
-first_name VARCHAR (30) NOT NULL,
-last_name VARCHAR (30) NOT NULL,
-role_id INT NOT NULL,
-manager_id INT,
-PRIMARY KEY (id),
-FOREIGN KEY (role_id) REFERENCES role(id),
-FOREIGN KEY (manager_id) REFERENCES employee(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
 );
 
 
--- seed.sql
+++ seed.sql
@@ -1,17 +1,15 @@
 USE employeeDB;
 -- Seed data for departments table:
-INSERT INTO department (name, id)
-VALUES ('Finance', 1), ('HR', 2), ('Sales', 3);
+INSERT INTO department (name)
+VALUES ('Finance'), ('HR'), ('Sales');
 
-USE employeeDB;
 -- Seed data for role table:
-INSERT INTO role (id, title, salary, dep_id)
-VALUES (1, 'Junior Manager', 40000.00, 3), (2, 'Manager', 47000.00, 1), (3, 'Senior Manager', 52000.00, 2);
+INSERT INTO role (title, salary, dep_id)
+VALUES ('Junior Manager', 40000.00, 3), ('Manager', 47000.00, 1), ('Senior Manager', 52000.00, 2);
 
-USE employeeDB;
 -- Seed data for employee table:
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Phil', 'Smetona', 2, 2), (2, 'Harriet', 'Johnson', 3, null), (3, 'Arnold', 'Srikar', 1, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Phil', 'Smetona', 2, 2), ('Harriet', 'Johnson', 3, null), ('Arnold', 'Srikar', 1, 2);
 
 SELECT * FROM department;
 SELECT * FROM role;
