6ff5bcc1b457fa13c25ef0da8147f4e59de2e6b5
rnick1 <rnick0260@gmail.com>
2021-03-17 09:32:43 -0700

    Edited the updateRole function.

--- app.js
+++ app.js
@@ -196,24 +196,44 @@ const addEmployee = () => {
     });
 }
 const updateRole = () => {
-    const query = connection.query(
-        'UPDATE role SET ? WHERE ?',
-        [
+    connection.query(`SELECT 
+    employee.id,
+    employee.first_name, 
+    employee.last_name, 
+    role.title, 
+    department.name AS Department, 
+    role.salary, 
+    employee.manager_id 
+    FROM role 
+    INNER JOIN employee on role.id = employee.role_id 
+    INNER JOIN department on department.id = role.dep_id;
+    `, (err, res) => {
+        inquirer.prompt([
             {
-                quantity: 100,
+                name: 'last_name',
+                type: 'input',
+                message: 'Please enter the last name of the employee you would like to edit:',
             },
             {
-                flavor: 'Rocky Road',
+                name: 'role_id',
+                type: 'input',
+                message: 'Please enter the id of this employee\'s new role:',
             },
-        ],
-        (err, res) => {
-            if (err) throw err;
-            console.log(`${res.affectedRows} products updated!\n`);
-            // Call deleteProduct AFTER the UPDATE completes
-            deleteProduct();
-        }
-    );
-
-    // logs the actual query being run
-    console.log(query.sql);
-};
+        ]).then(function (res) {
+            connection.query(
+                'UPDATE employee SET ? WHERE ?',
+                {
+                    role_id: res.role_id,
+                },
+                {
+                    last_name: res.last_name,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('This employee\'s information has been updated!');
+                    menu();
+                }
+            );
+        })
+    });
+}
